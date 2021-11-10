754767453f85a1279ca8285fa3a70261ec541444
sudban3089 <banerj24@msu.edu>
2021-11-10 09:02:22 +0530

    Update and rename PixelandPRNUFeatures_NDFI_set1.m to PixelandPRNUFeatures_TestSetNDFI.m

--- PixelandPRNUFeatures_TestSetNDFI.m
+++ PixelandPRNUFeatures_TestSetNDFI.m
@@ -2,17 +2,19 @@ clc
 close all
 clear
 
+%% This script extracts the pixel intensity features and sensor pattern noise (PRNU) features from the images of the NDFI test set
+
+% Make sure the folders Functions/ and Filter/ are loctaed in the current working diretcory for PRNU extraction
 addpath('Functions/')
 addpath('Filter/')
-
 qmf = MakeONFilter('Daubechies',8);
 L = 4;
 
 
-imageDir = 'S:\DependenceGraph_FaceImages\Dataset\SET I';
+imageDir = 'Path to NDFI SET I imagesa';
 cd(imageDir)
 images = dir('*.bmp');
-cd('S:\WVU_Multimodal\CODES')
+cd('Path to where /Filter and /Functions folders are located')
 for i=1:length(images)
     i
     filename = images(i).name;
@@ -20,7 +22,7 @@ for i=1:length(images)
         
     PixelFeatures(i,:) = image(:);
     
-    Noisex_fft = PhaseNoiseExtractFromImage_SUD_Enhanced(image,qmf,2,L);
+    Noisex_fft = PhaseNoiseExtractFromImage_Enhanced(image,qmf,2,L);
     %% use this for Basic SPN, enhanced Basic SPN 
     Noiseresidual_spatial = single(Noisex_fft);
     Noiseresidual_testimage = double(Noiseresidual_spatial);    
@@ -29,8 +31,8 @@ for i=1:length(images)
     FileName{i} = filename;
 end
     
-cd('S:\PGM Image Phylogeny\Codes\HGNN-master\datasets\FaceFeats_NDFI\SetI')
-save('PixelFeatures_Resized.mat','FileName','PixelFeatures')
-save('PRNUFeats_Resized.mat','PRNU_Features')
+cd('Results\FaceFeats_NDFI')
+save('PixelFeatures.mat','FileName','PixelFeatures')
+save('PRNUFeats.mat','PRNU_Features')
 clear images PixelFeatures FileName PRNU_Features
 
